2c8a623ab8d52438ba519af3800e358e046aba84
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-04-02 18:36:32 -0300

    tratamento de erro na digitação do usuario

--- Termo.ConsoleApp/Program.cs
+++ Termo.ConsoleApp/Program.cs
@@ -4,8 +4,41 @@
     {
         static void Main(string[] args)
         {
-                
-            Console.ReadLine();
+
+
+            while (true) //loop principal
+            {
+                string? palavraDigitada;
+                while (true) //loop para validação, verifica se o usuario esta jogando de acordo com as regras.
+                {
+                    Console.WriteLine("--------------------------");
+                    Console.WriteLine("Jogo Termo");
+                    Console.WriteLine("--------------------------");
+
+                    Console.WriteLine("");
+
+                    Console.Write(">");
+                    palavraDigitada = Console.ReadLine();
+
+                    if (palavraDigitada?.Length < 5 ||
+                       string.IsNullOrWhiteSpace(palavraDigitada) ||
+                       palavraDigitada.Any(char.IsDigit)||
+                       palavraDigitada?.Length > 5) // estudar o tratamenter de erro de caracteres.
+                    {
+                        Console.WriteLine("sua palavra deve ter no maximo 5 caracteres sem espaços ou numero");
+                        Console.WriteLine("\nDigite ENTER para digitar novamente...");
+                        Console.ReadLine();
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                Console.WriteLine(palavraDigitada);
+                Console.ReadLine();
+            }
         }
     }
 }
