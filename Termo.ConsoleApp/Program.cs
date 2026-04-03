e148ea285d4ffe9c9437bf1a99a11f32cc4da58f
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-04-03 03:25:04 -0300

    implementada a logica do jogo

--- Termo.ConsoleApp/Program.cs
+++ Termo.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Cryptography;
+﻿using System.Drawing;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
 
 namespace Termo.ConsoleApp
 {
@@ -24,41 +26,65 @@ namespace Termo.ConsoleApp
 
             string PalavraAleatoria = tiposDePalavras[EscolhaDaPalavra];//escolhe a palavra guardada no vetor e guarda em uma variavel para apresentala.
 
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Jogo Termo");
+            Console.WriteLine("--------------------------");
+
             while (true) //loop principal
             {
                 string? palavraDigitada;
-                while (true) //loop para validação, verifica se o usuario esta jogando de acordo com as regras.
+
+
+                Console.WriteLine();
+
+                Console.Write(">");
+                palavraDigitada = Console.ReadLine();
+
+
+                if (palavraDigitada?.Length != 5 ||
+                   string.IsNullOrWhiteSpace(palavraDigitada) ||
+                   !palavraDigitada.All(char.IsLetter)) //nesta linha esta tratando sobre o erro ao inserir numero e caractere
                 {
-                    Console.WriteLine("--------------------------");
-                    Console.WriteLine("Jogo Termo");
-                    Console.WriteLine("--------------------------");
-
-                    Console.WriteLine("");
-
-                    Console.Write(">");
-                    palavraDigitada = Console.ReadLine();
-
-
-                    if (palavraDigitada?.Length != 5 ||
-                       string.IsNullOrWhiteSpace(palavraDigitada) ||
-                      !palavraDigitada.All(char.IsLetter)) //nesta linha esta tratando sobre o erro ao inserir numero e caractere
-                    {
-                        Console.WriteLine("sua palavra deve ter no maximo 5 caracteres sem espaços, numeros ou caracteres especiais");
-                        Console.WriteLine("\nDigite ENTER para digitar novamente...");
-                        Console.ReadLine();
-                        continue;
-                    }
-                    else
-                    {
-                        break;
-                    }
-
-
-
+                    Console.WriteLine("sua palavra deve ter no maximo 5 caracteres sem espaços, numeros ou caracteres especiais");
+                    Console.WriteLine("\nDigite ENTER para digitar novamente...");
+                    Console.ReadLine();
+                    continue;
                 }
 
+                if (palavraDigitada != PalavraAleatoria) //caso nn tenha ganhado direto ele entra em outro criterio de verificação
+                {
+                    for (int i = 0; i < palavraDigitada.Length; i++)
+                    {
+                        char verificadorDeLetras = palavraDigitada[i];
 
-                Console.ReadLine();
+                        if (verificadorDeLetras == PalavraAleatoria[i]) //letra existe e esta na pósição correta
+                        {
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            Console.Write(verificadorDeLetras);
+                            Console.ResetColor();
+                        }
+                        else if (PalavraAleatoria.Contains(verificadorDeLetras)) //contains verifica se existe na palavra mas ele nn diz a posição e nem quantas vezes ele s erepete.
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkYellow;
+                            Console.Write(verificadorDeLetras);
+                            Console.ResetColor();
+                        }
+                        else //letra nn existe
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkRed;
+                            Console.Write(verificadorDeLetras);
+                            Console.ResetColor();
+                        }
+                    }
+                }
+                else if (palavraDigitada == PalavraAleatoria) // virifica se o usuario acertou em cheio e ganhou
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine(palavraDigitada);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    break;
+                }
             }
         }
     }
